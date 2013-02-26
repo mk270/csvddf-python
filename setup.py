8530a1eb0c1c2354f5413c90e58b5cc00f2f455f
Martin Keegan <martin@no.ucant.org>
2013-02-26 22:33:49 +0000

    sort out packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from setuptools import setup
+
+setup(
+	name = "csvddf",
+	author = "Martin Keegan",
+	author_email = "martin.keegan@okfn.org",
+	version = "1.0",
+	license = "Apache Licence v2.0",
+	url = "",
+	download_url = "",
+	description = "Python library for dealing with CSVDDF files",
+	py_modules = "",
+	packages = ["csvddf"],
+	install_requires = "",
+	scripts = ""
+)
